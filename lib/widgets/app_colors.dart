09990ce26679e03ac85be45a5a4f6640a314c6d6
Joaomarcellodev <joaomarcellobraz@gmail.com>
2025-04-30 23:00:13 -0300

    add images readme and img figma

--- lib/widgets/app_colors.dart
+++ lib/widgets/app_colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Appcolors {
-  static const Color background = Color.fromARGB(255, 0, 173, 195);
-  static const Color foreground = Color.fromARGB(255, 247, 247, 247);
-  static const Color surface = Color.fromARGB(255, 115, 207, 213);
-  static const Color surfaceAlternative = Color.fromARGB(255, 43, 74, 142);
-  static const Color border = Color.fromARGB(255, 183, 231, 238);
-  static const Color yellow = Color.fromARGB(255, 237, 199, 53);
+  static const Color background = Color.fromRGBO(0, 173, 195, 1);
+  static const Color foreground = Color(0xFFF7F7F7);
+  static const Color surface = Color.fromRGBO(115, 207, 213, 1);
+  static const Color surfaceAlternative = Color.fromRGBO(43, 74, 142, 1);
+  static const Color border = Color.fromRGBO(183, 231, 238, 1);
+  static const Color yellow = Color.fromRGBO(237, 199, 53, 1);
 }
